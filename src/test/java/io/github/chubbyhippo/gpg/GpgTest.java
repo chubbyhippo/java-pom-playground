c86c2d69c11ed6b41a52f4fb7c20e6c3d9549d80
chubbyhippo <mkmsacc@outlook.com>
2026-03-31 10:20:22 +0700

    Refactor `GpgTest` to fix inconsistent indentation

--- src/test/java/io/github/chubbyhippo/gpg/GpgTest.java
+++ src/test/java/io/github/chubbyhippo/gpg/GpgTest.java
@@ -45,155 +45,148 @@ import org.junit.jupiter.api.Test;
 
 public class GpgTest {
 
-  @BeforeAll
-  static void setUpProvider() {
-    Security.addProvider(new BouncyCastleProvider());
-  }
-
-  @Test
-  void shouldEncryptAndDecryptMessage() throws Exception {
-    var passphrase = "test-passphrase".toCharArray();
-    var original = "hello gpg";
-
-    var secretKeyRing = generateSecretKeyRing(passphrase);
-    var publicKey = findEncryptionKey(secretKeyRing);
-    var secretKey = findSecretKey(secretKeyRing);
-
-    var encrypted = encrypt(original.getBytes(StandardCharsets.UTF_8), publicKey);
-    var decrypted = decrypt(encrypted, secretKey, passphrase);
-
-    assertThat(new String(decrypted, StandardCharsets.UTF_8)).isEqualTo(original);
-  }
-
-  private static PGPSecretKeyRing generateSecretKeyRing(char[] passphrase) throws Exception {
-    var generator = KeyPairGenerator.getInstance("RSA", "BC");
-    generator.initialize(2048);
-
-    var keyPair = generator.generateKeyPair();
-    var pgpKeyPair =
-        new JcaPGPKeyPair(PublicKeyPacket.VERSION_4, PGPPublicKey.RSA_GENERAL, keyPair, new Date());
-
-    var sha1 = new JcaPGPDigestCalculatorProviderBuilder().build().get(HashAlgorithmTags.SHA1);
-
-    var keyRingGenerator =
-        new PGPKeyRingGenerator(
-            PGPSignature.POSITIVE_CERTIFICATION,
-            pgpKeyPair,
-            "test@example.com",
-            sha1,
-            null,
-            null,
-            new JcaPGPContentSignerBuilder(
-                    pgpKeyPair.getPublicKey().getAlgorithm(), HashAlgorithmTags.SHA256)
-                .setProvider("BC"),
-            new JcePBESecretKeyEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256, sha1)
-                .setProvider("BC")
-                .build(passphrase));
-
-    return keyRingGenerator.generateSecretKeyRing();
-  }
-
-  private static PGPPublicKey findEncryptionKey(PGPSecretKeyRing keyRing) {
-    var keys = keyRing.getPublicKeys();
-    while (keys.hasNext()) {
-      var key = keys.next();
-      if (key.isEncryptionKey()) {
-        return key;
-      }
+    @BeforeAll
+    static void setUpProvider() {
+        Security.addProvider(new BouncyCastleProvider());
     }
-    throw new IllegalStateException("No encryption key found");
-  }
 
-  private static PGPSecretKey findSecretKey(PGPSecretKeyRing keyRing) {
-    var keys = keyRing.getSecretKeys();
-    while (keys.hasNext()) {
-      var key = keys.next();
-      if (key.isSigningKey()) {
-        return key;
-      }
+    @Test
+    void shouldEncryptAndDecryptMessage() throws Exception {
+        var passphrase = "test-passphrase".toCharArray();
+        var original = "hello gpg";
+
+        var secretKeyRing = generateSecretKeyRing(passphrase);
+        var publicKey = findEncryptionKey(secretKeyRing);
+        var secretKey = findSecretKey(secretKeyRing);
+
+        var encrypted = encrypt(original.getBytes(StandardCharsets.UTF_8), publicKey);
+        var decrypted = decrypt(encrypted, secretKey, passphrase);
+
+        assertThat(new String(decrypted, StandardCharsets.UTF_8)).isEqualTo(original);
     }
-    throw new IllegalStateException("No secret key found");
-  }
 
-  private static byte[] encrypt(byte[] clearData, PGPPublicKey encryptionKey) throws Exception {
-    var output = new ByteArrayOutputStream();
+    private static PGPSecretKeyRing generateSecretKeyRing(char[] passphrase) throws Exception {
+        var generator = KeyPairGenerator.getInstance("RSA", "BC");
+        generator.initialize(2048);
 
-    try (var armoredOutput = new ArmoredOutputStream(output)) {
-      var compressedBuffer = new ByteArrayOutputStream();
-      var compressor = new PGPCompressedDataGenerator(CompressionAlgorithmTags.ZIP);
+        var keyPair = generator.generateKeyPair();
+        var pgpKeyPair = new JcaPGPKeyPair(PublicKeyPacket.VERSION_4, PGPPublicKey.RSA_GENERAL, keyPair, new Date());
 
-      try {
-        try (var compressedOut = compressor.open(compressedBuffer)) {
-          var literalGenerator = new PGPLiteralDataGenerator();
-          try (var literalOut =
-              literalGenerator.open(
-                  compressedOut, PGPLiteralData.BINARY, "data", clearData.length, new Date())) {
-            literalOut.write(clearData);
-          }
+        var sha1 = new JcaPGPDigestCalculatorProviderBuilder().build().get(HashAlgorithmTags.SHA1);
+
+        var keyRingGenerator = new PGPKeyRingGenerator(
+                PGPSignature.POSITIVE_CERTIFICATION,
+                pgpKeyPair,
+                "test@example.com",
+                sha1,
+                null,
+                null,
+                new JcaPGPContentSignerBuilder(pgpKeyPair.getPublicKey().getAlgorithm(), HashAlgorithmTags.SHA256)
+                        .setProvider("BC"),
+                new JcePBESecretKeyEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256, sha1)
+                        .setProvider("BC")
+                        .build(passphrase));
+
+        return keyRingGenerator.generateSecretKeyRing();
+    }
+
+    private static PGPPublicKey findEncryptionKey(PGPSecretKeyRing keyRing) {
+        var keys = keyRing.getPublicKeys();
+        while (keys.hasNext()) {
+            var key = keys.next();
+            if (key.isEncryptionKey()) {
+                return key;
+            }
         }
-      } finally {
-        compressor.close();
-      }
-
-      var compressedData = compressedBuffer.toByteArray();
-
-      var encryptedDataGenerator =
-          new PGPEncryptedDataGenerator(
-              new JcePGPDataEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256)
-                  .setWithIntegrityPacket(true)
-                  .setSecureRandom(new SecureRandom())
-                  .setProvider("BC"));
-
-      encryptedDataGenerator.addMethod(
-          new JcePublicKeyKeyEncryptionMethodGenerator(encryptionKey).setProvider("BC"));
-
-      try (var encryptedOut = encryptedDataGenerator.open(armoredOutput, compressedData.length)) {
-        encryptedOut.write(compressedData);
-      }
+        throw new IllegalStateException("No encryption key found");
     }
 
-    return output.toByteArray();
-  }
-
-  private static byte[] decrypt(byte[] encryptedData, PGPSecretKey secretKey, char[] passphrase)
-      throws Exception {
-    PGPObjectFactory factory =
-        new JcaPGPObjectFactory(PGPUtil.getDecoderStream(new ByteArrayInputStream(encryptedData)));
-
-    var object = factory.nextObject();
-    var encryptedDataList =
-        object instanceof PGPEncryptedDataList
-            ? (PGPEncryptedDataList) object
-            : (PGPEncryptedDataList) factory.nextObject();
-
-    var encryptedPacket =
-        (PGPPublicKeyEncryptedData) encryptedDataList.getEncryptedDataObjects().next();
-
-    var decryptor = new JcePBESecretKeyDecryptorBuilder().setProvider("BC").build(passphrase);
-
-    var privateKey = secretKey.extractPrivateKey(decryptor);
-
-    try (var clearStream =
-        encryptedPacket.getDataStream(
-            new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(privateKey))) {
-
-      var plainFactory = new JcaPGPObjectFactory(clearStream);
-      var message = plainFactory.nextObject();
-
-      if (message instanceof PGPCompressedData compressedData) {
-        var compressedFactory = new JcaPGPObjectFactory(compressedData.getDataStream());
-        message = compressedFactory.nextObject();
-      }
-
-      if (message instanceof PGPLiteralData literalData) {
-        return literalData.getInputStream().readAllBytes();
-      }
-
-      if (message instanceof PGPOnePassSignatureList) {
-        throw new PGPException("Signed messages are not handled in this test");
-      }
-
-      throw new PGPException("Unexpected PGP message type: " + message.getClass().getName());
+    private static PGPSecretKey findSecretKey(PGPSecretKeyRing keyRing) {
+        var keys = keyRing.getSecretKeys();
+        while (keys.hasNext()) {
+            var key = keys.next();
+            if (key.isSigningKey()) {
+                return key;
+            }
+        }
+        throw new IllegalStateException("No secret key found");
     }
-  }
+
+    private static byte[] encrypt(byte[] clearData, PGPPublicKey encryptionKey) throws Exception {
+        var output = new ByteArrayOutputStream();
+
+        try (var armoredOutput = new ArmoredOutputStream(output)) {
+            var compressedBuffer = new ByteArrayOutputStream();
+            var compressor = new PGPCompressedDataGenerator(CompressionAlgorithmTags.ZIP);
+
+            try {
+                try (var compressedOut = compressor.open(compressedBuffer)) {
+                    var literalGenerator = new PGPLiteralDataGenerator();
+                    try (var literalOut = literalGenerator.open(
+                            compressedOut, PGPLiteralData.BINARY, "data", clearData.length, new Date())) {
+                        literalOut.write(clearData);
+                    }
+                }
+            } finally {
+                compressor.close();
+            }
+
+            var compressedData = compressedBuffer.toByteArray();
+
+            var encryptedDataGenerator =
+                    new PGPEncryptedDataGenerator(new JcePGPDataEncryptorBuilder(SymmetricKeyAlgorithmTags.AES_256)
+                            .setWithIntegrityPacket(true)
+                            .setSecureRandom(new SecureRandom())
+                            .setProvider("BC"));
+
+            encryptedDataGenerator.addMethod(
+                    new JcePublicKeyKeyEncryptionMethodGenerator(encryptionKey).setProvider("BC"));
+
+            try (var encryptedOut = encryptedDataGenerator.open(armoredOutput, compressedData.length)) {
+                encryptedOut.write(compressedData);
+            }
+        }
+
+        return output.toByteArray();
+    }
+
+    private static byte[] decrypt(byte[] encryptedData, PGPSecretKey secretKey, char[] passphrase) throws Exception {
+        PGPObjectFactory factory =
+                new JcaPGPObjectFactory(PGPUtil.getDecoderStream(new ByteArrayInputStream(encryptedData)));
+
+        var object = factory.nextObject();
+        var encryptedDataList = object instanceof PGPEncryptedDataList
+                ? (PGPEncryptedDataList) object
+                : (PGPEncryptedDataList) factory.nextObject();
+
+        var encryptedPacket = (PGPPublicKeyEncryptedData)
+                encryptedDataList.getEncryptedDataObjects().next();
+
+        var decryptor = new JcePBESecretKeyDecryptorBuilder().setProvider("BC").build(passphrase);
+
+        var privateKey = secretKey.extractPrivateKey(decryptor);
+
+        try (var clearStream = encryptedPacket.getDataStream(
+                new JcePublicKeyDataDecryptorFactoryBuilder().setProvider("BC").build(privateKey))) {
+
+            var plainFactory = new JcaPGPObjectFactory(clearStream);
+            var message = plainFactory.nextObject();
+
+            if (message instanceof PGPCompressedData compressedData) {
+                var compressedFactory = new JcaPGPObjectFactory(compressedData.getDataStream());
+                message = compressedFactory.nextObject();
+            }
+
+            if (message instanceof PGPLiteralData literalData) {
+                return literalData.getInputStream().readAllBytes();
+            }
+
+            if (message instanceof PGPOnePassSignatureList) {
+                throw new PGPException("Signed messages are not handled in this test");
+            }
+
+            throw new PGPException(
+                    "Unexpected PGP message type: " + message.getClass().getName());
+        }
+    }
 }
