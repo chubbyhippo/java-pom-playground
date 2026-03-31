6ee1d831036a20b1c26d85235cc283b92eb8c158
chubbyhippo <mkmsacc@outlook.com>
2026-03-31 10:21:07 +0700

    Refactor `InstancioTest` to fix inconsistent indentation

--- src/test/java/io/github/chubbyhippo/instancio/InstancioTest.java
+++ src/test/java/io/github/chubbyhippo/instancio/InstancioTest.java
@@ -9,32 +9,30 @@ import org.junit.jupiter.api.Test;
 
 public class InstancioTest {
 
-  record Address(String street, String city) {}
+    record Address(String street, String city) {}
 
-  record User(Long id, String name, List<Address> addresses) {}
+    record User(Long id, String name, List<Address> addresses) {}
 
-  @Test
-  void testUserGeneration() {
-    // Create a fully populated User object with one line
-    User user = Instancio.create(User.class);
+    @Test
+    void testUserGeneration() {
+        // Create a fully populated User object with one line
+        User user = Instancio.create(User.class);
 
-    assertThat(user.id()).isNotNull();
-    assertThat(user.name()).isNotBlank();
-    assertThat(user.addresses()).isNotEmpty();
-  }
+        assertThat(user.id()).isNotNull();
+        assertThat(user.name()).isNotBlank();
+        assertThat(user.addresses()).isNotEmpty();
+    }
 
-  @Test
-  void testCustomizedUserGeneration() {
-    // You can also customize specific fields while letting the rest be random
-    User customUser =
-        Instancio.of(User.class)
-            .set(Select.field(User::name), "Jesus")
-            .generate(Select.field(Address::city), gen -> gen.oneOf("Nazareth", "Galilee"))
-            .create();
+    @Test
+    void testCustomizedUserGeneration() {
+        // You can also customize specific fields while letting the rest be random
+        User customUser = Instancio.of(User.class)
+                .set(Select.field(User::name), "Jesus")
+                .generate(Select.field(Address::city), gen -> gen.oneOf("Nazareth", "Galilee"))
+                .create();
 
-    assertThat(customUser.name()).isEqualTo("Jesus");
-    assertThat(customUser.addresses())
-        .extracting(Address::city)
-        .allMatch(city -> List.of("Nazareth", "Galilee").contains(city));
-  }
+        assertThat(customUser.name()).isEqualTo("Jesus");
+        assertThat(customUser.addresses()).extracting(Address::city).allMatch(city -> List.of("Nazareth", "Galilee")
+                .contains(city));
+    }
 }
