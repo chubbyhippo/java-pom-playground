175a4035183814cac4c14e44d3aea1aacd79a84c
chubbyhippo <mkmsacc@outlook.com>
2026-03-31 10:05:10 +0700

    Refactor `ApprovalVoidMethodTest` to fix inconsistent indentation

--- src/test/java/io/github/chubbyhippo/approval/ApprovalVoidMethodTest.java
+++ src/test/java/io/github/chubbyhippo/approval/ApprovalVoidMethodTest.java
@@ -2,27 +2,26 @@ package io.github.chubbyhippo.approval;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
 public class ApprovalVoidMethodTest {
 
-    @Test
-    void testVoidMethodOutput() {
-        var outputStream = new ByteArrayOutputStream();
-        var originalOut = System.out;
-        System.setOut(new PrintStream(outputStream));
+  @Test
+  void testVoidMethodOutput() {
+    var outputStream = new ByteArrayOutputStream();
+    var originalOut = System.out;
+    System.setOut(new PrintStream(outputStream));
 
-        try {
-            printHello();
-            Approvals.verify(outputStream.toString());
-        } finally {
-            System.setOut(originalOut);
-        }
+    try {
+      printHello();
+      Approvals.verify(outputStream.toString());
+    } finally {
+      System.setOut(originalOut);
     }
+  }
 
-    private void printHello() {
-        IO.println("Hello, Approval Tests!");
-    }
+  private void printHello() {
+    IO.println("Hello, Approval Tests!");
+  }
 }
