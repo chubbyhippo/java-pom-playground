acee276f8dcbf47b3de323d4cf7162a8784173c3
chubbyhippo <mkmsacc@outlook.com>
2026-04-16 17:08:57 +0700

    Refactor test class to use package-private visibility
    Changed the `ApprovalVoidMethodTest` class from public to package-private to encapsulate its visibility. This aligns with the convention of keeping test classes only available within the same package.

--- src/test/java/io/github/chubbyhippo/approval/ApprovalVoidMethodTest.java
+++ src/test/java/io/github/chubbyhippo/approval/ApprovalVoidMethodTest.java
@@ -5,7 +5,7 @@ import java.io.PrintStream;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
-public class ApprovalVoidMethodTest {
+class ApprovalVoidMethodTest {
 
     @Test
     void testVoidMethodOutput() {
